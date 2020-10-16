fe2591abf693298a553e8a290b4cb306d5deb276
Daniela Garofalo <daniela.garofalo@mail.polimi.it>
2020-10-16 19:18:21 +0200

    First implementation of the Rx_Interrupt header and source files

--- GAROFALO_DANIELA.cydsn/Rx_Interrupt.c
+++ GAROFALO_DANIELA.cydsn/Rx_Interrupt.c
@@ -10,4 +10,19 @@
  * ========================================
 */
 
+/*
+    File Rx_Interrupt.c
+    Function aimed to reset the timer counting when a byte from the serial port is received
+    Author: Daniela Garofalo
+*/
+    
+#include <Rx_Interrupt.h>
+
+int flag_received;
+
+CY_ISR(custom_UART_ISR)
+{
+    flag_received = 1; // Flag variable that indicates the receiving of the byte
+}
+    
 /* [] END OF FILE */
--- GAROFALO_DANIELA.cydsn/Rx_Interrupt.h
+++ GAROFALO_DANIELA.cydsn/Rx_Interrupt.h
@@ -10,4 +10,18 @@
  * ========================================
 */
 
+/*
+    File Rx_Interrupt.h
+    Function aimed to the definition of the ISR function generated when a byte is received from the serial port
+    Author: Daniela Garofalo
+*/
+
+#ifndef __RX_INTERRUPT_H__
+    #define __RX_INTERRUPT_H__
+   
+    #include <cytypes.h>
+    
+    CY_ISR_PROTO(custom_UART_ISR);
+#endif
+
 /* [] END OF FILE */
