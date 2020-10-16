147dcab2713ea4ec2277bb9b3905faf418371390
Daniela Garofalo <daniela.garofalo@mail.polimi.it>
2020-10-16 18:50:17 +0200

    Implementation of the INCLUDE and of the DEFINE in the main function

--- GAROFALO_DANIELA.cydsn/main.c
+++ GAROFALO_DANIELA.cydsn/main.c
@@ -10,6 +10,21 @@
  * ========================================
 */
 #include "project.h"
+#include <Timer_1.h>
+#include <UART_1.h>
+#include <Timer_Interrupt.h>
+#include <Rx_Interrupt.h>
+
+// Definition of different status values
+#define IDLE 0
+#define HEADERBYTE_RECEIVED 1
+#define REDBYTE_RECEIVED 2
+#define GREENBYTE_RECEIVED 3
+#define BLUEBYTE_RECEIVED 4
+#define TAILBYTE_RECEIVED 5
+
+#define HEADER 0xA0 // Value representing the beginning of the message
+#define TAIL 0xC0 // Value representing the end of the message
 
 int main(void)
 {
